65c6d7c604b24b0d7855f4e65cc9811c923bb0ab
Pavel Sterin <codeatstake@googlemail.com>
2012-05-30 21:06:50 +0200

    epic fail, continued

--- projekt.c
+++ projekt.c
@@ -7,213 +7,16 @@
 #include <assert.h>
 #include <stdint.h>
 
-#include <cairo.h>
+//#include <cairo.h>
+
+#include "expr.h"
+#include "splitop.h"
+#include "cvect.h"
+#include "carray.h"
 
 /*************************************************************************************************/
 
-typedef struct carray {
-  int          alloc;
-  int          length;
-  fftw_complex data[];
-  // field elements are allocated after the array
-} carray_t;
-
-carray_t * carray_new_sized(int length, int alloc) {
-  assert(alloc > length);
-  carray_t * r = malloc(sizeof(carray_t) + sizeof(fftw_complex) * alloc);
-  if (!r) { abort(); }
-  r->length = length;
-  r->alloc = length;
-  return r;
-}
-
-carray_t * carray_append(carray_t * z, fftw_complex x) {
-  if (z->length < z->alloc) {
-    z->data[z->length++] = x;
-    return z;
-  } else {
-    int alloc = (z->length+1) * 2;
-    carray_t * t = realloc(z, sizeof(carray_t) + sizeof(fftw_complex) * alloc); assert(t);
-    t->data[t->length++] = x;
-    t->alloc = alloc;
-    return t;
-  }
-}
-
-/*************************************************************************************************/
-
-double cabssq(fftw_complex z) {
-  double x = creal(z), y = cimag(z);
-  return x * x + y * y;
-}
-
-double cvect_normsq(int len, fftw_complex a[len]) {
-  double A = 0;
-  for (int k = 0; k < len; k++) A += cabssq(a[k]);
-  return A;
-}
-
-fftw_complex cvect_skp(int len, fftw_complex * a, fftw_complex * b) {
-  fftw_complex c = 0;
-  for (; len > 0 ; len--, a++, b++) {
-    c += conj(*a)* *b;
-  }
-  return c;
-}
-
-/*************************************************************************************************/
-
-typedef struct {
-  int            bins;
-  fftw_plan      fwd;
-  fftw_plan      bwd;
-  double       * V;
-  fftw_complex * apsi;
-  fftw_complex * eV;
-  fftw_complex * eVn;
-  fftw_complex * ehV;
-  fftw_complex * ehVn;
-  fftw_complex * eT;
-  double         dt;
-  double         dk;
-  double         dx;
-  double         L;
-  double         min, max;
-} splitop_t;
-
-splitop_t * splitop_new(int bins, double dt, double min, double max, fftw_complex (*V)(double x)) {
-  assert(bins > 1 && min < max);
-  splitop_t * w = malloc(sizeof(splitop_t)); assert(w);
-  w->bins = bins;
-  w->min = min;
-  w->max = max;
-  w->L = max - min;
-  w->dt = dt;
-  w->dx = w->L / bins;
-  w->dk = 2*M_PI/w->L;
-  w->V = fftw_malloc(sizeof(fftw_complex) * bins); assert(w->V);
-  for (int n = 0; n < bins; n++) {
-    w->V[n] = V(min + w->dx*n);
-  }
-  w->eV = fftw_alloc_complex(bins); assert(w->eV);
-  w->eVn = fftw_alloc_complex(bins); assert(w->eVn);
-  w->ehV = fftw_alloc_complex(bins); assert(w->ehV);
-  w->ehVn = fftw_alloc_complex(bins); assert(w->ehVn);
-  for (int n = 0; n < bins; n++) {
-    w->eV[n] = cexp(-I * w->V[n] * w->dt);
-    w->eVn[n] = w->eV[n] / bins;
-    w->ehV[n] = cexp(-I * w->V[n] * w->dt / 2);
-    w->ehVn[n] = w->ehV[n] / bins;
-  }
-  w->eT = fftw_alloc_complex(bins); assert(w->eT);
-  for (int n = 0; n < bins; n++) {
-    if (n < bins/2) {
-      w->eT[n] = cexp(-I * w->dk * w->dk * n * n * w->dt);
-    } else {
-      int m = (n - bins);
-      w->eT[n] = cexp(-I * w->dk * w->dk * m * m * w->dt);
-    }
-  }
-  w->fwd = fftw_plan_dft_1d(bins, NULL, NULL, FFTW_FORWARD, FFTW_ESTIMATE);
-  w->bwd = fftw_plan_dft_1d(bins, NULL, NULL, FFTW_BACKWARD, FFTW_ESTIMATE);
-  return w;
-}
-
-void splitop_free(splitop_t * w) {
-  fftw_destroy_plan(w->fwd);
-  fftw_destroy_plan(w->bwd);
-  fftw_free(w->V);
-  fftw_free(w->eV);
-  fftw_free(w->eVn);
-  fftw_free(w->ehV);
-  fftw_free(w->ehVn);
-  fftw_free(w->eT);
-  fftw_free(w->apsi);
-  free(w);
-}
-
-#include <emmintrin.h>
-#include <smmintrin.h>
-
-/* a *= z */
-inline static void cvect_mult_asign(int len, fftw_complex * a, fftw_complex * z) {
-  //for (int n = 0; n < len; n++) { a[n] *= z[n]; }
-  for (int n = 0; n < len; n++, a++, z++) {
-    double * va = (double *) a;
-    double * vz = (double *) z;
-    // (a0,a0)
-    register __m128d tmp0 = _mm_load1_pd(va);
-    // (b0,b1)
-    register __m128d tmp1 = _mm_load_pd(vz);
-    // (a1,a1)
-    register __m128d tmp2 = _mm_load1_pd(va+1);
-    // (b1,b0)
-    register __m128d tmp3 = _mm_shuffle_pd(tmp1, tmp1, 1);
-    // (a0 b0,a0 b1)
-    register __m128d tmp4 = _mm_mul_pd(tmp0, tmp1);
-    // (a1 b1,a1 b0)
-    register __m128d tmp5 = _mm_mul_pd(tmp2, tmp3);
-    register __m128d tmp6 = _mm_addsub_pd(tmp4, tmp5);
-    _mm_store_pd(va, tmp6);
-  }
-}
-
-void splitop_run(splitop_t * w, int times, fftw_complex * psi) {
-  fftw_complex * psik = fftw_alloc_complex(w->bins); assert(psik);
-  int bins = w->bins;
-  fftw_complex * ehV = w->ehV;
-  fftw_complex * eVn = w->eVn;
-  fftw_complex * ehVn = w->ehVn;
-  fftw_complex * eT = w->eT;
-
-  /*for (int k = 0; k < times; k++) {
-    //for (int n = 0; n < bins; n++) { psi[n] *= ehV[n]; }
-    for (p = psi, as = ehV, ae = as + bins; as < ae; as++, p++) { (*p) *= *as; }
-    fftw_execute_dft(w->fwd, psi, psik);
-    //for (int n = 0; n < bins; n++) { psik[n] *= eT[n]; }
-    for (p = psik, as = eT, ae = as + bins; as < ae; as++, p++) { (*p) *= *as; }
-    fftw_execute_dft(w->bwd, psik, psi);
-    //for (int n = 0; n < bins; n++) { psi[n] *= ehV[n] / bins; }
-    for (p = psi, as = ehV, ae = as + bins; as < ae; as++, p++) { (*p) *= *as/bins; }
-  }*/
-
-  if (times > 0) {
-    cvect_mult_asign(bins, psi, ehV);
-    fftw_execute_dft(w->fwd, psi, psik);
-    cvect_mult_asign(bins, psik, eT);
-    for (int k = 0; k < times - 1; k++) {
-      fftw_execute_dft(w->bwd, psik, psi);
-      cvect_mult_asign(bins, psi, eVn);
-      fftw_execute_dft(w->fwd, psi, psik);
-      cvect_mult_asign(bins, psik, eT);
-    }
-    fftw_execute_dft(w->bwd, psik, psi);
-    cvect_mult_asign(bins, psi, ehVn);
-  }
-
-  fftw_free(psik);
-}
-
-fftw_complex * splitop_prepare(splitop_t * w, fftw_complex (*fn)(double x)) {
-  fftw_complex * psi = fftw_alloc_complex(w->bins); assert(psi);
-  for (int n = 0; n < w->bins; n++) {
-    psi[n] = fn(w->min + w->dx * n);
-  }
-  double A = cvect_normsq(w->bins, psi);
-  for (int n = 0; n < w->bins; n++) {
-    psi[n] *= 1/sqrt(A);
-  }
-  return psi;
-}
-
-void splitop_save(splitop_t * w, fftw_complex * spsi) {
-  fftw_complex * psi = fftw_alloc_complex(w->bins); assert(psi);
-  for (int n = 0; n < w->bins; n++) {
-    psi[n] = spsi[n];
-  }
-  w->apsi = psi;
-}
-
+#if 0
 void splitop_draw(splitop_t * w, cairo_t * cr, cairo_rectangle_t rect, fftw_complex * psi) {
   cairo_save(cr);
   cairo_rectangle(cr, rect.x, rect.y, rect.width, rect.height);
@@ -300,6 +103,7 @@ void splitop_draw(splitop_t * w, cairo_t * cr, cairo_rectangle_t rect, fftw_comp
 
   cairo_restore(cr);
 }
+#endif
 
 /*************************************************************************************************/
 
@@ -415,7 +219,9 @@ fftw_complex fn(double x) {
 }
 #endif
 
-int main(/*int argc, char *argv[]*/) {
+
+#if 0
+int nomain(/*int argc, char *argv[]*/) {
   fftw_init_threads();
   fftw_plan_with_nthreads(2);
 
@@ -540,5 +346,31 @@ int main(/*int argc, char *argv[]*/) {
   splitop_free(sop);
   fftw_cleanup_threads();
   fftw_cleanup();
+  return 0;
+}
+#endif
+
+int main() {
+  char buf[] =
+    "b = 1;"
+    "a = b;"
+    "u = 2 + a;"
+    "bar(x,y,z):=x^y^z;"
+    "f(x,y,z):=z*y*x + bar(1,2,b) + u;"
+    "g(x,y,z):=z*y*x + bar(3,2,2) + u;"
+    "d = 3;"
+    //"f(x):=(a+b-c*123)^43645.234 + 2e6;"
+    ;
+  token_t * tokens = exprlexer_tokenize(buf);
+  ast_t * ast = ast_new(tokens);
+
+  funcdef_t * fn = ast_get_func(ast, "g");
+
+  double args[3] = {3,3,3};
+
+  ast_eval(ast, fn, 3, args);
+
+  ast_free(ast);
+
   return 0;
 }
